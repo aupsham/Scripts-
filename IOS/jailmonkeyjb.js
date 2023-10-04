4f661ed293716bfe531333f983fb67665cadc09a
Ashish Upsham <44950379+aupsham@users.noreply.github.com>
2023-10-04 11:58:19 +0530

    Delete IOS/jailmonkeyjb.js
    no need

--- IOS/jailmonkeyjb.js
+++
@@ -1,9 +0,0 @@
-/**
-Root detection bypass script for Gantix JailMoney
-**/
-const klass = ObjC.classes.JailMonkey;
-Interceptor.attach(klass['- isJailBroken'].implementation, {
-    onLeave: function (retval) {
-        retval.replace(0);
-    }
-});
